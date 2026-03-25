cb37e3e6f50b7e2ff767cc2ad35c9203d000d39a
Raficknew <rafi20302@gmail.com>
2026-03-25 13:47:06 +0100

    feat: Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-background w-full h-screen px-20 py-22'>
+      <div className='h-full rounded-md bg-foreground px-10 py-22'>
+        <main>
+          <h1>Initial setup</h1>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
