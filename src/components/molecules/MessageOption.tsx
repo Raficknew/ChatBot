47830c79f991d580898a43f0e85789a7993125fc
Raficknew <rafi20302@gmail.com>
2026-03-27 10:41:46 +0100

    fix: icon positioning on messageOption

--- src/components/molecules/MessageOption.tsx
+++ src/components/molecules/MessageOption.tsx
@@ -1,22 +1,17 @@
 import type { MessageOptionType } from "../../types/types";
 import { IconButton } from "../atoms/IconButton";
 
-export function MessageOption({
-  title,
-  icon,
-  onClick,
-
-}: MessageOptionType) {
+export function MessageOption({ title, icon, onClick }: MessageOptionType) {
   return (
     <button
       type="button"
-      className="w-full sm:flex-1 sm:max-w-[270px] sm:h-[213px] min-h-32 bg-foreground px-4 py-4 pb-10 sm:px-6 sm:py-9 cursor-pointer text-start relative flex flex-col justify-start inverted"
+      className="w-full md:flex-1 md:max-w-[270px] min-h-32 md:min-h-[213px] bg-foreground px-4 py-4 pb-10 sm:px-6 sm:py-9 cursor-pointer text-start relative flex flex-col justify-start inverted"
       onClick={() => {
         onClick?.();
       }}
     >
       <h2 className="text-text-secondary text-sm sm:text-base">{title}</h2>
-      <div className="absolute bottom-2 right-2 sm:bottom-0 sm:right-2">
+      <div className="absolute bottom-0 right-0">
         <IconButton className="bg-foreground rounded-lg border border-surface">
           {icon}
         </IconButton>
